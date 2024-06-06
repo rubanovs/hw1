4f0d8fcedea8e9de79a5b93a05c14761aa9ad3b5
rubanovs <112646924+rubanovs@users.noreply.github.com>
2024-06-06 16:53:57 +0300

    Update protocol.py

--- protocol.py
+++ protocol.py
@@ -1,13 +1,12 @@
 import socket
-import time
-from queue import PriorityQueue
+import threading
+import collections
 
-HEADER_SIZE = 16
-TIMEOUT = 0.01
 
 class UDPBasedProtocol:
     def __init__(self, *, local_addr, remote_addr):
-        self.udp_socket = socket.socket(family=socket.AF_INET, type=socket.SOCK_DGRAM)
+        self.udp_socket = socket.socket(
+            family=socket.AF_INET, type=socket.SOCK_DGRAM)
         self.remote_addr = remote_addr
         self.udp_socket.bind(local_addr)
 
@@ -17,152 +16,193 @@ class UDPBasedProtocol:
     def recvfrom(self, n):
         msg, addr = self.udp_socket.recvfrom(n)
         return msg
-        
+
     def close(self):
         self.udp_socket.close()
 
 
-
-class Segment:
-
-    def __init__(self, seq: int, ack: int, data: bytes):
-        self.data = data
-        self.seq = seq
-        self.ack = ack
-        self.send_time = time.time()
-        self.isConnected = False
-
-    def __len__(self):
-        return len(self.data)
-
-    def __eq__(self, other):
-        return self.seq == other.seq
-    
-    def __lt__(self, other):
-        return self.seq < other.seq
-
-
-    def encode(self) -> bytes:
-        return self.seq.to_bytes(8, "big", signed=False) + self.ack.to_bytes(8, "big", signed=False) + self.data
-    
-    @staticmethod
-    def decode(data: bytes) -> 'Segment':
-        return Segment(int.from_bytes(data[:8], "big", signed=False), int.from_bytes(data[8:16], "big", signed=False), data[HEADER_SIZE:])
-
-    @property
-    def timeodOut(self):
-        if not self.isConnected and (time.time() - self.send_time > TIMEOUT):
-            return True
-        return False
-
-
-
 class MyTCPProtocol(UDPBasedProtocol):
-    def __init__(self, client='Client', *args, **kwargs):
+    def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        self.buff_size = 1024
-        self.window_size = 5000
-        self.client = client
-        self.sent_byte = 0
-        self.recv_byte = 0
-        self.ack_bytes = 0
-        self.buf = bytes()
-        self.send_window = PriorityQueue()
-        self.recv_window = PriorityQueue()
+        self.udp_socket.settimeout(0.0001)
+        self.seq = 0
+        self.ack = 0
+        self.saved_ack_data = b''
+        self.saved_receive_data = b''
+        self.saved_data_lock = threading.Lock()
+        self.buffer_size = 10000
+        self.active = True
+        self.send_lock = threading.Condition()
+        self.recv_lock = threading.Condition()
+        self.recv_result_lock = threading.Condition()
+        self.ack_check_lock = threading.Lock()
+        self.data_to_send = collections.deque()
+        self.num_to_receive = 0
+        self.received_data = b''
 
+        self.send_thread = threading.Thread(target=self.send_call)
+        self.recv_thread = threading.Thread(target=self.recv_call)
+        self.send_thread.daemon = True
+        self.recv_thread.daemon = True
+        self.send_thread.start()
+        self.recv_thread.start()
 
-    def SendSegment(self, segment: Segment) -> int:
-        self.udp_socket.settimeout(None)
-        bytes_sent = self.sendto(segment.encode()) - HEADER_SIZE
+    @staticmethod
+    def create_packet(num_of_bytes, seq, data):
+        return int.to_bytes(1, 1, 'big') + int.to_bytes(num_of_bytes, 4, 'big') + int.to_bytes(seq, 4, 'big') + data
 
-        if len(segment):
-            segment.data = segment.data[: bytes_sent]
-         
-            segment.send_time = time.time()
-            self.send_window.put((segment.seq, segment), block=False)
+    @staticmethod
+    def create_message_with_ack(ack):
+        return int.to_bytes(0, 1, 'big') + int.to_bytes(ack, 4, 'big')
 
+    @staticmethod
+    def parse_message_with_ack(data):
+        return int.from_bytes(data[0: 1], 'big'), int.from_bytes(data[1: 5], 'big')
 
-        if segment.seq == self.sent_byte:
-            self.sent_byte += bytes_sent
+    @staticmethod
+    def parse_message_with_data(data):
+        return int.from_bytes(data[0: 1], 'big'), int.from_bytes(data[1: 5], 'big'), int.from_bytes(data[5: 9], 'big'), data[9: len(data)]
 
-        return bytes_sent
+    def send_call(self):
+        current_length_of_data = -1
+        while self.active:
 
-    def send(self, data: bytes) -> int:
-        counter = 0
-        ack_attempts = 0
-        while (data or self.ack_bytes < self.sent_byte):
-            if self.sent_byte - self.ack_bytes <= self.window_size and data:
-                end = min(self.buff_size, len(data))
-                data_size = self.SendSegment(Segment(self.sent_byte, self.recv_byte, data[: end]))
-                data = data[data_size:]
-                counter += data_size
-                self.RecvSegment(0.)
-            else:
-                if self.RecvSegment(TIMEOUT):
-                    ack_attempts = 0
-                else:
-                    ack_attempts += 1
+            data = b''
+            with self.send_lock:
+                while len(self.data_to_send) == 0 and self.active:
+                    self.send_lock.wait()
 
-            if not self.send_window.empty():
-                _, first_segment = self.send_window.get(block=False)
-                if first_segment.timeodOut:
-                    self.SendSegment(first_segment)
-                else:
-                    self.send_window.put((first_segment.seq, first_segment), block=False)
-
-
-        return counter
-
-
-
-    def RecvSegment(self, timeout: float = None) -> bool:
-        self.udp_socket.settimeout(timeout)
-        try:
-            segment = Segment.decode(self.recvfrom(self.buff_size + HEADER_SIZE))
-        except socket.error:
-            return False
-
-        if len(segment):
-            self.recv_window.put((segment.seq, segment), block=False)
-            first_segment = None
-            while not self.recv_window.empty():
-                _, first_segment = self.recv_window.get(block=False)
-                if first_segment.seq < self.recv_byte:
-                    first_segment.isConnected = True
-                elif first_segment.seq == self.recv_byte:
-                    self.buf += first_segment.data
-                    self.recv_byte += len(first_segment)
-                    first_segment.isConnected = True
-                else:
-                    self.recv_window.put((first_segment.seq, first_segment), block=False)
+                if not self.active:
                     break
 
-            if first_segment is not None:
-                self.SendSegment(Segment(self.sent_byte, self.recv_byte, bytes()))
+                if current_length_of_data == -1:
+                    current_length_of_data = len(self.data_to_send[0])
 
+                length = min(
+                    len(self.data_to_send[0]), self.buffer_size)
+                data += self.data_to_send[0][0:length]
+                self.data_to_send[0] = self.data_to_send[0][length:]
 
-        if segment.ack > self.ack_bytes:
-            self.ack_bytes = segment.ack
-            while not self.send_window.empty():
-                _, first_segment = self.send_window.get(block=False)
-                if first_segment.seq >= self.ack_bytes:
-                    self.send_window.put((first_segment.seq, first_segment), block=False)
+                packet = MyTCPProtocol.create_packet(
+                    current_length_of_data, self.seq, data)
+
+                if len(self.data_to_send[0]) == 0:
+                    self.data_to_send.popleft()
+                    current_length_of_data = -1
+
+            while True:
+                final_flag = False
+                interim = 0
+                while True:
+                    if not self.active:
+                        return
+                    interim += 1
+                    interim = min(interim, 1)
+                    for _ in range(interim):
+                        self.sendto(packet)
+                    while True:
+                        received = b''
+                        try:
+                            received = self.recvfrom(self.buffer_size + 10)
+                        except Exception as e:
+                            with self.ack_check_lock:
+                                if len(self.saved_ack_data) != 0:
+                                    received = self.saved_ack_data
+                                    self.saved_ack_data = b''
+                                else:
+                                    break
+                        flag, ack = MyTCPProtocol.parse_message_with_ack(
+                            received)
+                        if flag != 0:
+                            with self.saved_data_lock:
+                                self.saved_receive_data = received
+                            continue
+
+                        if ack > self.seq:
+                            self.seq = ack
+                            final_flag = True
+                            break
+
+                        break
+                    if final_flag:
+                        break
+                if final_flag:
                     break
 
-        return True
-    
+    def send(self, data: bytes):
+        with self.send_lock:
+            self.data_to_send.append(data)
+            self.send_lock.notify_all()
+        return len(data)
 
-    def recv(self, n: int) -> bytes:
-        end = min(n, len(self.buf))
-        data = self.buf[:end]
-        self.buf = self.buf[end:]
-        while len(data) < n:
-            self.RecvSegment()
-            end = min(n, len(self.buf))
-            data += self.buf[:end]
-            self.buf = self.buf[end:]
+    def recv_call(self):
 
-        return data
+        while self.active:
+            result = b''
+            with self.recv_lock:
+                while self.active and self.num_to_receive == 0:
+                    self.recv_lock.wait()
+                if not self.active:
+                    break
+            prev_ack = self.ack
+            while True:
+                try:
+                    received = self.recvfrom(self.buffer_size + 10)
+                except Exception as e:
+                    with self.saved_data_lock:
+                        if len(self.saved_receive_data) != 0:
+                            received = self.saved_receive_data
+                            self.saved_receive_data = b''
+                        else:
+                            continue
+                flag, num_of_bytes, seq, data = MyTCPProtocol.parse_message_with_data(
+                    received)
+                if flag != 1:
+                    with self.ack_check_lock:
+                        self.saved_ack_data = received
+                    continue
+
+                if seq != self.ack or (len(data) != self.buffer_size and len(data) != num_of_bytes - (self.ack - prev_ack)):
+
+                    message = MyTCPProtocol.create_message_with_ack(
+                        self.ack)
+                    self.sendto(message)
+                else:
+                    self.ack += len(data)
+                    result += data
+                    message = MyTCPProtocol.create_message_with_ack(
+                        self.ack)
+                    self.sendto(message)
+                if self.ack - prev_ack == num_of_bytes:
+                    for _ in range(5):
+                        self.sendto(message)
+                    break
+
+            self.received_data = result
+            self.num_to_receive = 0
+            with self.recv_result_lock:
+                self.recv_result_lock.notify_all()
+        return result
+
+    def recv(self, n: int):
+        result = bytes()
+        with self.recv_lock:
+            self.num_to_receive = n
+            self.recv_lock.notify_all()
+        with self.recv_result_lock:
+            while len(self.received_data) == 0:
+                self.recv_result_lock.wait()
+
+                result += self.received_data
+                self.received_data = b''
+                return result
 
     def close(self):
+        self.active = False
+        with self.send_lock:
+            self.send_lock.notify_all()
+        with self.recv_lock:
+            self.recv_lock.notify_all()
+        self.send_thread.join()
+        self.recv_thread.join()
         super().close()
